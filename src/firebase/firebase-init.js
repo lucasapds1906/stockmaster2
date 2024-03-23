d1d2802a99274f3459f171a918798df541b003b4
Lucas Silva <lucasapds1906@outlook.com>
2024-03-23 18:56:16 -0300

    teste

---
+++ src/firebase/firebase-init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAcyix30Q2sq3z6noB8aXDaqbT6PGH-jK0",
+  authDomain: "stockmaster-b4801.firebaseapp.com",
+  projectId: "stockmaster-b4801",
+  storageBucket: "stockmaster-b4801.appspot.com",
+  messagingSenderId: "92702621720",
+  appId: "1:92702621720:web:218be64b102d415ac2436d",
+  measurementId: "G-WHGGXHQ5NF"
+};
+
+firebase.initializeApp(firebaseConfig);
