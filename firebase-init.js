18420bd24dd19f3872ec838186f97ecd289a700e
Lucas Silva <lucasapds1906@outlook.com>
2024-03-23 00:04:07 -0300

    first commit

---
+++ firebase-init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCvbL0PzsT5O2aJpfg9TANfpzKca5NSSPE",
+    authDomain: "stockmaster-9d77b.firebaseapp.com",
+    projectId: "stockmaster-9d77b",
+    storageBucket: "stockmaster-9d77b.appspot.com",
+    messagingSenderId: "919880979892",
+    appId: "1:919880979892:web:a082a06c6ba3e25b914ee1",
+    measurementId: "G-J5XXHLJSM1"
+};
+
+firebase.initializeApp(firebaseConfig);
